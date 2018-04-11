ba69743d61c67c1c2dc175d9c9a15b2a70f636f5
jpall <jpall@jpalldeMacBook-Pro.local>
2018-04-11 11:41:30 +0800

    update index.php

--- backend/wp-content/themes/mingde/index.php
+++ backend/wp-content/themes/mingde/index.php
@@ -3,8 +3,122 @@
 get_header();
 ?>
 
+    <div class="container-fluid">
+        <div class="row">
+            <img class="img-responsive" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/banner_1.jpg" alt="">
+        </div>
+    </div>
 
-<?php get_template_part('template','home') ?>
+    <!-- 产品及服务 -->
+    <div class="container home-project-and-service">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="item-title">
+                    <div class="title-line">
+                        <h3>产品及服务</h3>
+                        <small>Products and services</small>
+                        <label>more</label>
+                    </div>
+                    <div class="bottom-line"></div>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="pro-item">
+                    <img class="img-responsive" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/home_img_1.png" alt="">
+                    <span>企业投贷联动咨询及服务</span>
+                    <small>联合母基金</small>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="pro-item">
+                    <img class="img-responsive" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/home_img_2.png" alt="">
+                    <span>PE/并购夹层基金和服务</span>
+                    <small>实现优质投资</small>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <div class="pro-item">
+                    <img class="img-responsive" src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/home_img_3.png" alt="">
+                    <span>企业投资并购咨询及服务</span>
+                    <small>提供投资并购合作</small>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- 企业家评价 -->
+    <div class="container-fluid entrepreneur-evaluation">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="item-title">
+                        <div class="title-line">
+                            <h3>企业家评价</h3>
+                            <small>Evaluate</small>
+                        </div>
+                        <div class="bottom-line"></div>
+                    </div>
+                </div>
+                <div class="col-md-12">
+                    <div class="enter-item">
+                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/home_img_4.png" alt="">
+                        <div class="enter-text">
+                            <span>明心资本协助客户筛选交易对手、开展尽职调查、设计交易方案、协助交易谈判和整合等一揽子综合性财务顾问服务， 十分专业，为公司带来不止一倍的收益。</span>
+                            <label>————链家创始人-左晖</label>
+                        </div>
+                    </div>
+                    <div class="enter-item">
+                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/home_img_5.png" alt="">
+                        <div class="enter-text">
+                            <span>明心资本协助客户筛选交易对手、开展尽职调查、设计交易方案、协助交易谈判和整合等一揽子综合性财务顾问服务 十分专业，为公司带来不止一倍的收益。</span>
+                            <label>————链家创始人-蔡红亮</label>
+                        </div>
+                    </div>
+                    <div class="enter-item">
+                        <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/home_img_6.png" alt="">
+                        <div class="enter-text">
+                            <span>明心资本协助客户筛选交易对手、开展尽职调查、设计交易方案、协助交易谈判和整合等一揽子综合性财务顾问服务 十分专业，为公司带来不止一倍的收益。</span>
+                            <label>————链家创始人-王健林</label>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <!-- 合作伙伴 -->
+    <div class="container partner">
+        <div class="row">
+            <div class="col-md-12">
+                <div class="item-title">
+                    <div class="title-line">
+                        <h3>合作伙伴</h3>
+                        <small>Cooperative partner</small>
+                        <span>more</span>
+                    </div>
+                    <div class="bottom-line"></div>
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="partner-logo">
+                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/link_1.png" alt="">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="partner-logo">
+                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/link_2.png" alt="">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="partner-logo">
+                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/link_3.png" alt="">
+                </div>
+            </div>
+            <div class="col-md-3">
+                <div class="partner-logo">
+                    <img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/link_4.png" alt="">
+                </div>
+            </div>
+        </div>
+    </div>
 
 <?php get_footer(); ?>
