0d3baec4f5efb5d9e6931e4bc4e5d8f2cd011055
jpall <jpall@jpalldeMacBook-Pro.local>
2018-05-04 16:30:40 +0800

    fix

--- backend/wp-content/themes/wojia/template/project.php
+++ backend/wp-content/themes/wojia/template/project.php
@@ -43,7 +43,7 @@ get_header();
         <div class="row">
                 <div class="col-md-4">
                     <div class="pro-item">
-                        <img src="<?php the_post_thumbnail() ?>" >
+                        <?php the_post_thumbnail() ?>
                         <div class="mask">
                             <label><?php the_title()?></label>
                         </div>
